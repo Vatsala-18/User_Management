cfa499b63a69b9bb1690e321f641d8aac28f43c3
jagdeep.khatana <khatanajagdeep1999@gmail.com>
2023-05-11 20:32:59 +0530

    IntegerArrayType support for postgresql

--- openvidu_db-backend/src/main/java/com/openvidu_databases/openvidu_dbbackend/Utils/GenericArrayUserType.java
+++ openvidu_db-backend/src/main/java/com/openvidu_databases/openvidu_dbbackend/Utils/GenericArrayUserType.java
@@ -1,5 +1,6 @@
 package com.openvidu_databases.openvidu_dbbackend.Utils;
 
+import org.hibernate.HibernateException;
 import org.hibernate.engine.spi.SharedSessionContractImplementor;
 import org.hibernate.usertype.UserType;
 import org.springframework.stereotype.Component;
@@ -10,94 +11,66 @@ import java.util.Objects;
 
 @Component
 public class GenericArrayUserType implements UserType {
-    private static final int[] SQL_TYPES = {Types.ARRAY };
+  @Override
+  public int[] sqlTypes() {
+    return new int[]{Types.ARRAY};
+  }
 
-    private final Class<?> clazz;
+  @Override
+  public Class returnedClass() {
+    return Integer[].class;
+  }
 
+  @Override
+  public boolean equals(Object o, Object o1) throws HibernateException {
+    return false;
+  }
 
-    public GenericArrayUserType(Class<?> clazz) {
-        this.clazz = clazz;
+  @Override
+  public int hashCode(Object o) throws HibernateException {
+    return 0;
+  }
+
+  @Override
+  public Object nullSafeGet(ResultSet rs, String[] names, SharedSessionContractImplementor session, Object owner)
+    throws HibernateException, SQLException {
+    Array array = rs.getArray(names[0]);
+    return array != null ? array.getArray() : null;
+  }
+
+  @Override
+  public void nullSafeSet(PreparedStatement st, Object value, int index, SharedSessionContractImplementor session)
+    throws HibernateException, SQLException {
+    if (value != null && st != null) {
+      Array array = session.connection().createArrayOf("int", (Integer[])value);
+      st.setArray(index, array);
+    } else {
+      st.setNull(index, sqlTypes()[0]);
     }
+  }
 
-    @Override
-    public int[] sqlTypes() {
-        return SQL_TYPES;
-    }
+  @Override
+  public Object deepCopy(Object o) throws HibernateException {
+    return null;
+  }
 
-    @Override
-    public Class<?> returnedClass() {
-        return clazz;
-    }
+  @Override
+  public boolean isMutable() {
+    return false;
+  }
 
-    @Override
-    public boolean equals(Object o, Object o1) {
-        return Objects.equals(o, o1);
-    }
+  @Override
+  public Serializable disassemble(Object o) throws HibernateException {
+    return null;
+  }
 
-    @Override
-    public int hashCode(Object o) {
-        return Objects.hashCode(o);
-    }
+  @Override
+  public Object assemble(Serializable serializable, Object o) throws HibernateException {
+    return null;
+  }
 
-    @Override
-    public Object nullSafeGet(ResultSet resultSet, String[] strings, SharedSessionContractImplementor sharedSessionContractImplementor, Object o) throws SQLException {
-        if (resultSet.wasNull()) {
-            return null;
-        }
-
-        Array array = resultSet.getArray(strings[0]);
-        if (array == null) {
-            return null;
-        }
-
-        Object[] javaArray = (Object[]) array.getArray();
-        Object[] newArray = (Object[]) java.lang.reflect.Array.newInstance(clazz.getComponentType(), javaArray.length);
-
-        System.arraycopy(javaArray, 0, newArray, 0, javaArray.length);
-        return newArray;
-    }
-
-    @Override
-    public void nullSafeSet(PreparedStatement preparedStatement, Object o, int i, SharedSessionContractImplementor sharedSessionContractImplementor) throws SQLException {
-        if (o == null) {
-            preparedStatement.setNull(i, SQL_TYPES[0]);
-        } else {
-            Object[] castObject = (Object[]) o;
-            Array array = sharedSessionContractImplementor.connection().createArrayOf("integer", castObject);
-            preparedStatement.setArray(i, array);
-        }
-    }
-
-    @Override
-    public Object deepCopy(Object o) {
-        if (o == null) {
-            return null;
-        }
-
-        Object[] castObject = (Object[]) o;
-        Object[] newArray = (Object[]) java.lang.reflect.Array.newInstance(clazz.getComponentType(), castObject.length);
-        System.arraycopy(castObject, 0, newArray, 0, castObject.length);
-        return newArray;
-    }
-
-    @Override
-    public boolean isMutable() {
-        return true;
-    }
-
-    @Override
-    public Serializable disassemble(Object o) {
-        return (Serializable) o;
-    }
-
-    @Override
-    public Object assemble(Serializable serializable, Object o) {
-        return serializable;
-    }
-
-    @Override
-    public Object replace(Object o, Object o1, Object o2) {
-        return o;
-    }
-
+  @Override
+  public Object replace(Object o, Object o1, Object o2) throws HibernateException {
+    return null;
+  }
 }
